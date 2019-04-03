6752da5446776c3d7bdc611a9b1e89a607679981
Aiman Nadzmi <aiman_nadzmi@hotmail.com>
2019-04-02 21:32:20 -0700

    updated

---
+++ client/src/components/MessagingPage/Messaging.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Messaging = () => {
+    <div className="container">Mail Box
+        <div className="col-lg-6" id="friendList">Contacts</div>
+        <div className="col-lg-6" id="messageBox">Messages</div>
+    </div>
+}
+
+export default Messaging;
