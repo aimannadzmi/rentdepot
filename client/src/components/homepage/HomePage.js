e694bb08503993ffe97af4260935966db80034da
Aiman Nadzmi <aiman_nadzmi@hotmail.com>
2019-04-04 13:34:04 -0700

    minor updates

--- client/src/components/homepage/HomePage.js
+++ client/src/components/homepage/HomePage.js
@@ -3,6 +3,7 @@ import "./HomePage.css";
 
 const HomePage = () => (
     <div className="container">
+    <span className="rentItemText">Rent An Item</span>
         <div className="col-lg-12" id="showItems">
         </div>
     </div>
