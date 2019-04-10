7fcb542424fef2f8093bfcb2fff79ef558e2bd47
xkeekyx <xkeyanna@yahoo.com>
2019-04-10 13:44:31 -0700

    footer should show up

--- client/src/components/Search/Search.js
+++ client/src/components/Search/Search.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./Search.css";
 
-const Search = () => (
-    <div className="container">
-        <div className="col-lg-12" id="searchBar">
-            <input type="text" placeholder="Search" style={{width:"600px", marginTop: "20px"}}/>
-            <button className="col-lg-3" id="searchBtn">Search</button>
-        </div>
-    </div>
-)
-
+const Search = () => {
+    return (
+        <form className="form-inline active-cyan-4 center">
+            <input className="form-control form-control-sm mr-3 w-75" type="text" placeholder="Search" aria-label="Search"/>
+                <i className="fas fa-search" aria-hidden="true"></i>
+        </form>
+    )
+}
 export default Search;
--- client/src/components/homepage/HomePage.js
+++ client/src/components/homepage/HomePage.js
@@ -1,19 +1,22 @@
 import React from "react";
 import "./HomePage.css";
 import searchicon from "../../components/images/searchicon.png"
+import Search from "../Search/Search.js"
 
 const HomePage = () => {
     return (
         <div className="container">
-
-            <div className="row" id="homePageRow1">
+        <div className="col-md-8">
+            <Search/>
+            </div>
+            {/* <div className="row" id="homePageRow1">
                 <div className="col-lg-12" id="searchForm">
                     <form className="searchForm" id="searchBar">
                         <input className="searchField" type="search" placeholder="Search" aria-label="Search" />
                         <button className="searchSubmit" type="submit"><img id="searchicon" src={searchicon}/></button>
                     </form>
                 </div>
-            </div>
+            </div> */}
             <div className="row" id="homePageRow2">
                 <div className="col-lg-12" id="showItems">
                     <div className="container"><br></br>
