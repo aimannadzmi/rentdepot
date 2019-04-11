812978c1024c337a835a75563de5c2a55ac7805a
Aiman Nadzmi <aiman_nadzmi@hotmail.com>
2019-04-10 17:17:35 -0700

    minor addons and move arounds, check it outgit status

--- client/src/components/homepage/HomePage.js
+++ client/src/components/homepage/HomePage.js
@@ -4,6 +4,7 @@ import "./HomePage.css";
 import Search from "../Search/Search.js";
 import firebase from "firebase"
 import axios from "axios"
+import RentList from "../RentList/RentList"
 
 class HomePage extends Component {
     state = {
@@ -26,79 +27,16 @@ class HomePage extends Component {
         <div className="container">
 
             <Search />
-
-            <div className="row" id="homePageRow2">
-                <div className="col-lg-3" id="showItems">
-                    <div className="container"><br></br>
-                        <div className="col-lg-4" id="rentImage">
-                            <img src=""></img>
-                        </div>
-                        <div className="col-lg-8" id="rentDetails">
-                        </div>
-
-                    </div>
-                </div>
-
-                <div className="col-lg-3" id="showItems">
-                    <div className="container"><br></br>
-                        <div className="col-lg-4" id="rentImage">
-                            <img src=""></img>
-                        </div>
-                        <div className="col-lg-8" id="rentDetails">
-                        </div>
-
-                    </div>
-                </div>
-
-                <div className="col-lg-3" id="showItems">
-                    <div className="container"><br></br>
-                        <div className="col-lg-4" id="rentImage">
-                            <img src=""></img>
-                        </div>
-                        <div className="col-lg-8" id="rentDetails">
-                        </div>
-
-                    </div>
-                </div>
-
-                <div className="col-lg-3" id="showItems">
-                    <div className="container"><br></br>
-                        <div className="col-lg-4" id="rentImage">
-                            <img src=""></img>
-                        </div>
-                        <div className="col-lg-8" id="rentDetails">
-                        </div>
-
-                    </div>
-                </div>
-
-                <div className="col-lg-3" id="showItems">
-                    <div className="container"><br></br>
-                        <div className="col-lg-4" id="rentImage">
-                            <img src=""></img>
-                        </div>
-                        <div className="col-lg-8" id="rentDetails">
-                        </div>
-
-                    </div>
-                </div>
-
-                <div className="col-lg-3" id="showItems">
-                    <div className="container"><br></br>
-                        <div className="col-lg-4" id="rentImage">
-                            <img src=""></img>
-                        </div>
-                        <div className="col-lg-8" id="rentDetails">
-                         {this.state.items.map(item => 
-                         <li>{item.itemName}, {item.itemPrice}</li>
+            
+            {this.state.items.map(item => 
+                         <RentList itemName={item.itemName}
+                                 itemPrice={item.itemPrice}
+                                 itemLocation={item.itemLocation}
+                                 itemDescription={item.itemDescription}
+                                 itemImage={item.itemImage}/>
                          )}
-                        </div>
-
-                    </div>
-                </div>
                 
             </div>
-        </div>
     );
 }
 }
