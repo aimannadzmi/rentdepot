d0711aafe6be5e4c0d5e3a34a4564b02e3aa6855
xkeekyx <xkeyanna@yahoo.com>
2019-04-23 20:27:06 -0700

    building

---
+++ client/build/precache-manifest.209bb21ae6411f67d3c8fecac7d6efc3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "daebc2b3cfc8a3b3166bd946b5e13f1a",
+    "url": "/static/media/logo.daebc2b3.png"
+  },
+  {
+    "revision": "daebc2b3cfc8a3b3166bd946b5e13f1a",
+    "url": "/static/media/logo.daebc2b3.jpg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8f827b6d0e16a8bb2cec",
+    "url": "/static/js/main.b88c627e.chunk.js"
+  },
+  {
+    "revision": "269b8213393d7c2a21fa",
+    "url": "/static/js/2.549f0caa.chunk.js"
+  },
+  {
+    "revision": "8f827b6d0e16a8bb2cec",
+    "url": "/static/css/main.b7f53911.chunk.css"
+  },
+  {
+    "revision": "269b8213393d7c2a21fa",
+    "url": "/static/css/2.6464d87e.chunk.css"
+  },
+  {
+    "revision": "0084033f16bf11ba955dd556aac170f9",
+    "url": "/index.html"
+  }
+];
